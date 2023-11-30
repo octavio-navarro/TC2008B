ad1d9eabac1ed9237b0daf548d630d245e1a7a39
Octavio Navarro <octavio.navarro@tec.mx>
2023-11-30 03:07:17 -0600

    Added the create db statement.

--- CompetitionSite/db_Schema.sql
+++ CompetitionSite/db_Schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `TC2008B_Contest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `TC2008B_Contest`;
 -- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
 -- Host: localhost    Database: TC2008B_Contest
@@ -127,4 +129,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-30  2:54:25
+-- Dump completed on 2023-11-30  3:06:35
