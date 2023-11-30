0e97520ede16dcbd1f12babe68996beae760fc0a
Octavio Navarro <octavio.navarro@tec.mx>
2023-11-30 02:55:21 -0600

    Updated the database.

--- CompetitionSite/db_Schema.sql
+++ CompetitionSite/db_Schema.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `TC2008B_Contest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `TC2008B_Contest`;
 -- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
 -- Host: localhost    Database: TC2008B_Contest
@@ -33,7 +31,7 @@ CREATE TABLE `Attempts` (
   UNIQUE KEY `Attempts_ID_UNIQUE` (`Attempt_ID`),
   KEY `fk_Attempts_1_idx` (`Team_ID`),
   CONSTRAINT `fk_Attempts_1` FOREIGN KEY (`Team_ID`) REFERENCES `Teams` (`Team_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +48,7 @@ CREATE TABLE `Teams` (
   `Team_name` varchar(45) NOT NULL,
   PRIMARY KEY (`Team_ID`),
   UNIQUE KEY `Team_ID_UNIQUE` (`Team_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +68,21 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = @saved_cs_client;
 
 --
+-- Temporary view structure for view `avg_attempts`
+--
+
+DROP TABLE IF EXISTS `avg_attempts`;
+/*!50001 DROP VIEW IF EXISTS `avg_attempts`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `avg_attempts` AS SELECT 
+ 1 AS `Year`,
+ 1 AS `Classroom`,
+ 1 AS `Team`,
+ 1 AS `Average`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Final view structure for view `all_attempts`
 --
 
@@ -86,6 +99,24 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `avg_attempts`
+--
+
+/*!50001 DROP VIEW IF EXISTS `avg_attempts`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `avg_attempts` AS select `all_attempts`.`Team_year` AS `Year`,`all_attempts`.`Team_classroom` AS `Classroom`,`all_attempts`.`Team_name` AS `Team`,avg(`all_attempts`.`attempt_num_cars`) AS `Average` from `all_attempts` group by `all_attempts`.`Team_year`,`all_attempts`.`Team_classroom`,`all_attempts`.`Team_name` order by `all_attempts`.`Team_year`,`all_attempts`.`Team_classroom`,`all_attempts`.`Team_name` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -96,4 +127,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-29  0:42:11
+-- Dump completed on 2023-11-30  2:54:25
