2f5316c5e02562be8fdd8266621276f0d3cd2602
Octavio Navarro <octavio.navarro@tec.mx>
2024-11-28 12:09:41 -0600

    Fixed

--- CompetitionSite/db_Schema.sql
+++ CompetitionSite/db_Schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `TC2008B_Contest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `TC2008B_Contest`;
 -- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
 -- Host: localhost    Database: TC2008B_Contest
@@ -130,4 +132,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-28 12:08:34
+-- Dump completed on 2024-11-28 12:09:29
