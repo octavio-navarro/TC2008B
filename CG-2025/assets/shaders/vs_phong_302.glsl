33b85a689a09768015b0a8a971b4db54455464e2
Gilberto Echeverria <gilbertoef@gmail.com>
2025-11-16 16:44:29 -0600

    Update files for lighting examples!

--- CG-2025/assets/shaders/vs_phong_302.glsl
+++ CG-2025/assets/shaders/vs_phong_302.glsl
@@ -3,26 +3,33 @@ in vec4 a_position;
 in vec3 a_normal;
 
 // Scene uniforms
-uniform vec3 u_viewWorldPosition;
 uniform vec3 u_lightWorldPosition;
+uniform vec3 u_viewWorldPosition;
 
 // Model uniforms
 uniform mat4 u_world;
 uniform mat4 u_worldInverseTransform;
 uniform mat4 u_worldViewProjection;
 
-// Pass the transformed vectors to the fragment shader
+// Transformed normals
 out vec3 v_normal;
-out vec3 v_lightDirection;
-out vec3 v_cameraDirection;
+out vec3 v_surfaceToLight;
+out vec3 v_surfaceToView;
 
 void main() {
+    // Transform the position of the vertices
     gl_Position = vec4(0, 0, 0, 1);
 
-    // Apply the object transformations to the normal vector
+    // Transform the normal vector along with the object
     v_normal = a_normal;
 
-    // Get vectors to the light and to the camera
-    v_lightDirection = vec3(0, 0, 0);
-    v_cameraDirection = vec3(0, 0, 0);
+    // Get world position of the surface
+    vec3 surfaceWoldPosition = vec3(0, 0, 0);
+
+
+    // Direction from the surface to the light
+    v_surfaceToLight = vec3(0, 0, 0);
+
+    // Direction from the surface to the view
+    v_surfaceToView = vec3(0, 0, 0);
 }
